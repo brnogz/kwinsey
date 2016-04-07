d1b89259149206a9d429e9e6f7c695b9867314b0
baran oğuz <brn.oguz@gmail.com>
2016-04-07 22:25:21 +0300

    Output::write() returns 500 on null value now

--- src/Output.php
+++ src/Output.php
@@ -11,8 +11,14 @@ namespace kwinsey;
 
 class Output
 {
-    public static function write(Response $response)
+    public static function write(Response $response = null)
     {
+        if (is_null($response)) {
+            $response = new Response();
+            $response->setStatusCode(500);
+            $response->setData("Response cannot be null");
+        }
+        
         http_response_code($response->getStatusCode());
         echo json_encode($response->getData());
     }
