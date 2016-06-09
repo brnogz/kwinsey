a37f670c9415ed9ac72e9ea8f7f17a45491f9d8a
baran oğuz <brn.oguz@gmail.com>
2016-06-09 20:23:27 +0300

    input class does not using native php post anymore

--- src/Input.php
+++ src/Input.php
@@ -24,15 +24,57 @@ class Input
 
     public function __construct()
     {
-        if ($this->requestMethod() == 'POST') {
-            $this->body = $this->escapeArray($_POST);
-        } else {
-            parse_str(file_get_contents("php://input"),$this->body);
-        }
+        $this->parseBody(file_get_contents("php://input"));
         $this->header = $this->escapeArray(getallheaders());
 
         $urlParser = UrlParser::getInstance(Application::getInstance()->getConfiguration());
         $this->get = $this->escapeArray($urlParser->getQuery());
+    }
+
+    /**
+     * @param string $input
+     * @return array
+     */
+    private function parseBody(string $input):array
+    {
+        $matches = null;
+        $input = urldecode($input);
+        preg_match_all('(([a-zA-Z0-9_]+([\[][a-zA-Z0-9_]*[\]])*)(=([a-zA-Z0-9\s%.,;:]*))?)', $input, $matches, PREG_SET_ORDER);
+
+        $tBody = [];
+        foreach ($matches as $match) {
+            $indexes = [];
+            preg_match_all('([\[][a-zA-Z0-9_]*[\]])', $match[1], $indexes);
+
+            if (($end = strpos($match[1], '[')) !== false) {
+                $key = substr($match[1], 0, $end);
+            } else {
+                $key = $match[1];
+            }
+
+            $temp = $match[4];
+            $curIndexes = $indexes[0];
+            if (count($curIndexes) > 1 || !empty($curIndexes)) {
+                for ($i = count($curIndexes) - 1; $i >= 0; $i--) {
+                    $index = str_replace(['[', ']'], '', $curIndexes[$i]);
+                    if (!empty($index)) {
+                        $t2 = [$index => $temp];
+                    } else {
+                        $t2 = [$temp];
+                    }
+                    $temp = $t2;
+                }
+            }
+            if (isset($tBody[$key])) {
+                $tBody[$key] = array_merge_recursive($tBody[$key], $temp);
+            } else {
+                $tBody[$key] = $temp;
+            }
+        }
+
+        $this->body = $tBody;
+
+        return $tBody;
     }
 
     /**
@@ -45,15 +87,6 @@ class Input
             return $this->body;
         else
             return isset($this->body[$key]) ? $this->body[$key] : null;
-    }
-
-    /**
-     * @deprecated 
-     * @param string|null $key
-     * @return array|mixed|null
-     */
-    public function post(string $key = null){
-        return $this->body($key);
     }
 
     /**
