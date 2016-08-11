ad5d4b5cca72b3d27a2fa822afce726152de9bec
baran oğuz <brn.oguz@gmail.com>
2016-08-11 21:40:52 +0300

    registrar accepts hook class name instead of instance

--- src/hook/Registrar.php
+++ src/hook/Registrar.php
@@ -9,23 +9,30 @@
 namespace kwinsey\hook;
 
 
+use kwinsey\helper\Log;
+
 class Registrar
 {
     const PRE = 'pre';
     const POST = 'post';
     private static $hooks = ['pre' => [], 'post' => []];
 
-    public static function newHook(string $when, string $controller, Hook &$hook)
+    public static function newHook(string $when, string $controllerClass, string $controllerMethod, string $hook)
     {
-        static::$hooks[$when][$controller][] = &$hook;
+        static::$hooks[$when]['\\' . $controllerClass . '\\' . $controllerMethod][] = '\\' . $hook;
     }
 
     public static function runHooksIfExist(string $when, string $controller, $params = null)
     {
         if (isset(static::$hooks[$when][$controller]))
-            foreach (static::$hooks[$when][$controller] as &$hook) {
-                /** @var Hook $hook */
-                $hook->run($params);
+            foreach (static::$hooks[$when][$controller] as $hook) {
+                try {
+                    /** @var Hook $hookInstance */
+                    $hookInstance = new $hook();
+                    $hookInstance->run($params);
+                } catch (\Exception $e) {
+                    Log::e($e);
+                }
             }
     }
 }
