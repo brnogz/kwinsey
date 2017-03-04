966a8f4e19c58e262eaff7ec8cd9fc17e94f5b12
brnogz <brn.oguz@gmail.com>
2017-03-04 13:09:36 +0300

    fixed: undefined index error at log helper

--- src/helper/Log.php
+++ src/helper/Log.php
@@ -23,6 +23,14 @@ class Log
         static::log("---------");
     }
 
+    private static function log($str)
+    {
+        /** @var Config $config */
+        $config = Application::getInstance()->getConfiguration();
+
+        error_log('[' . date('Y-m-d H:i:s') . ']' . $str . PHP_EOL, 3, $config->general->log_loc);
+    }
+
     /**
      * @param \Throwable $e
      */
@@ -30,17 +38,15 @@ class Log
     {
         static::log($e->getMessage());
         foreach ($e->getTrace() as $traceLine) {
+            $traceLine['args'] = !isset($traceLine['args']) || empty($traceLine['args']) ? null : $traceLine['args'];
+            $traceLine['file'] = !isset($traceLine['file']) || empty($traceLine['file']) ? null : $traceLine['file'];
+            $traceLine['line'] = !isset($traceLine['line']) || empty($traceLine['line']) ? null : $traceLine['line'];
+            $traceLine['class'] = !isset($traceLine['class']) || empty($traceLine['class']) ? null : $traceLine['class'];
+            $traceLine['type'] = !isset($traceLine['type']) || empty($traceLine['type']) ? null : $traceLine['type'];
+
             $args = str_replace("\n", "", var_export($traceLine['args'], true));
-            static::log(@"{$traceLine['file']}({$traceLine['line']}): {$traceLine['class']}{$traceLine['type']}({$args})");
+            static::log("{$traceLine['file']}({$traceLine['line']}): {$traceLine['class']}{$traceLine['type']}({$args})");
         }
         static::log("---------");
-    }
-
-    private static function log($str)
-    {
-        /** @var Config $config */
-        $config = Application::getInstance()->getConfiguration();
-
-        error_log('[' . date('Y-m-d H:i:s') . ']' . $str . PHP_EOL, 3, $config->general->log_loc);
     }
 }
