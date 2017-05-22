5c98b56b8d468c1572ce6b7fb5853c76f25b4cee
brnogz <brn.oguz@gmail.com>
2017-05-22 18:51:53 +0300

    medoo path fixed

--- src/Model.php
+++ src/Model.php
@@ -23,7 +23,7 @@ abstract class Model
 
     public function __construct()
     {
-        $this->db = Medoo::getInstance();
+        $this->db = \Medoo\Medoo::getInstance();
     }
 
     public function getError()
